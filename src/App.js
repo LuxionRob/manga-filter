94df66422f3de405c37616c9335da0a7d41f6beb
LuxioRob <aidaynhi8@gmail.com>
2022-08-05 16:49:02 +0700

    Add tailwind, antd

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className="container flex"> Hello </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.jsx",
+    "./src/components/*/*.jsx",
+    "./src/pages/**/*.jsx",
+    "./src/layouts/**/*.jsx",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
